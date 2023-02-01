0c7827545616f4b4cefb28246a5528dc96ba1460
Ivan Riiako <52213412+Nogtbator@users.noreply.github.com>
2023-02-01 16:57:56 +0200

    7

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,31 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const refs = {
+  gallery: document.querySelector("div.gallery"),
+  
+//   btnCreateBoxes: document.querySelector("button[data-create]"),
+//   btnDestroyBoxes: document.querySelector("button[data-destroy]"),
+};
+
+
+createPaletteItems();
+function createPaletteItems() {
+    const items = galleryItems.map(
+      (item) => `<div class="gallery__item">
+  <a class="gallery__link" href="large-image.jpg">
+    <img
+      class="gallery__image"
+      src="${item.original}"
+      data-source="${item.preview}"
+      alt="${item.description}"
+    />
+  </a>
+</div>`).join('');
+    refs.gallery.insertAdjacentHTML("beforeend",  items);
+}
+
 
 console.log(galleryItems);
+
+
+
